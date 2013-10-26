bdb7cef169c93d213d47bd5ebb1d6ebc8415ba83
funnythingz <hiroki@hackleberry.me>
2013-10-26 13:15:11 +0900

    added Entity test

--- ddd-test.js
+++ ddd-test.js
@@ -39,6 +39,30 @@ var DDD;
 (function (DDD) {
     var expect = chai.expect;
 
+    describe("Entity", function () {
+        var id_1 = new DDD.Identity(1);
+        var id_2 = new DDD.Identity(2);
+
+        var entity_1 = new DDD.Entity(id_1);
+        var entity_2 = new DDD.Entity(id_2);
+
+        it("should be entity_1 has id_1", function () {
+            expect(entity_1.getIdentity()).to.equals(id_1);
+        });
+
+        it("should be entity_1 equals to entity_1", function () {
+            expect(entity_1.equals(entity_1)).to.be.true;
+        });
+
+        it("should be entity_1 not equals to entity_2", function () {
+            expect(entity_1.equals(entity_2)).to.not.be.true;
+        });
+    });
+})(DDD || (DDD = {}));
+;var DDD;
+(function (DDD) {
+    var expect = chai.expect;
+
     describe("Identity", function () {
         it("should be string identity has `id_hoge`", function () {
             var stringIdentity = new DDD.Identity('id_hoge');
---
+++ tests/ddd/entity.ts
@@ -0,0 +1,33 @@
+/// <reference path="../../definitions/mocha.d.ts" />
+/// <reference path="../../definitions/chai.d.ts" />
+
+/// <reference path="../../src/ddd/entity.ts" />
+/// <reference path="../../src/ddd/identity.ts" />
+
+module DDD {
+
+    var expect = chai.expect;
+
+    describe("Entity", function() {
+
+        var id_1: Identity<number> = new Identity(1);
+        var id_2: Identity<number> = new Identity(2);
+
+        var entity_1: Entity = new Entity(id_1);
+        var entity_2: Entity = new Entity(id_2);
+
+        it("should be entity_1 has id_1", function() {
+            expect(entity_1.getIdentity()).to.equals(id_1);
+        });
+
+        it("should be entity_1 equals to entity_1", function() {
+            expect(entity_1.equals(entity_1)).to.be.true;
+        });
+
+        it("should be entity_1 not equals to entity_2", function() {
+            expect(entity_1.equals(entity_2)).to.not.be.true;
+        });
+
+    });
+
+}
