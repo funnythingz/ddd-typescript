a671afe54a048d0519bec85e866f72728d54e5cd
funnythingz <hiroki@hackleberry.me>
2013-11-09 22:30:18 +0900

    Fixed test

--- ddd-test.js
+++ ddd-test.js
@@ -72,3 +72,51 @@ var DDD;
         });
     });
 })(DDD || (DDD = {}));
+;var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var DDD;
+(function (DDD) {
+    var expect = chai.expect;
+
+    var Pokemon = (function (_super) {
+        __extends(Pokemon, _super);
+        function Pokemon(identity) {
+            _super.call(this, identity);
+        }
+        return Pokemon;
+    })(DDD.Entity);
+    DDD.Pokemon = Pokemon;
+
+    var PokemonID = (function (_super) {
+        __extends(PokemonID, _super);
+        function PokemonID(value) {
+            _super.call(this, value);
+        }
+        return PokemonID;
+    })(DDD.Identity);
+    DDD.PokemonID = PokemonID;
+
+    describe("Pokemon", function () {
+        var id_001 = new PokemonID('001');
+        var pokemon_1 = new Pokemon(id_001);
+
+        var id_002 = new PokemonID('002');
+        var pokemon_2 = new Pokemon(id_002);
+
+        it("should be pokemon_1 has id_001", function () {
+            expect(pokemon_1.getIdentity()).to.equals(id_001);
+        });
+
+        it("should be pokemon_1 equals to pokemon_1", function () {
+            expect(pokemon_1.equals(pokemon_1)).to.be.true;
+        });
+
+        it("should be pokemon_1 not equals to pokemon_2", function () {
+            expect(pokemon_1.equals(pokemon_2)).to.not.be.true;
+        });
+    });
+})(DDD || (DDD = {}));
---
+++ tests/ddd/pokemon-spec.ts
@@ -0,0 +1,49 @@
+/// <reference path="../../definitions/mocha.d.ts" />
+/// <reference path="../../definitions/chai.d.ts" />
+
+/// <reference path="../../src/ddd/entity.ts" />
+/// <reference path="../../src/ddd/identity.ts" />
+
+module DDD {
+
+    var expect = chai.expect;
+
+    export class Pokemon extends Entity<Identity<string>> {
+
+        constructor(identity: Identity<string>) {
+            super(identity);
+        }
+
+    }
+
+    export class PokemonID extends Identity<string> {
+
+        constructor(value: string) {
+            super(value);
+        }
+
+    }
+
+    describe("Pokemon", function() {
+
+        var id_001: PokemonID =  new PokemonID('001');
+        var pokemon_1: Pokemon = new Pokemon(id_001);
+
+        var id_002: PokemonID =  new PokemonID('002');
+        var pokemon_2: Pokemon = new Pokemon(id_002);
+
+        it("should be pokemon_1 has id_001", function() {
+            expect(pokemon_1.getIdentity()).to.equals(id_001);
+        });
+
+        it("should be pokemon_1 equals to pokemon_1", function() {
+            expect(pokemon_1.equals(pokemon_1)).to.be.true;
+        });
+
+        it("should be pokemon_1 not equals to pokemon_2", function() {
+            expect(pokemon_1.equals(pokemon_2)).to.not.be.true;
+        });
+
+    });
+
+}
